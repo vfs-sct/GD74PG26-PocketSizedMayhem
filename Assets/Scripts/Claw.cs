2049918cf246cf48fb19b624c4da23bc47ec9df1
DenizhanYagci <denizhan1999@hotmail.com>
2024-08-29 15:33:17 -0700

    Added Mallet and Mallet Swing Animations

--- Assets/Scripts/Claw.cs
+++ Assets/Scripts/Claw.cs
@@ -1,59 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UIElements;
 
 public class Claw : Weapon
 {
-    [SerializeField] private GameObject grabbedObject;
-    [SerializeField] private FixedJoint joint;
-    [SerializeField] private bool fired = false;
-    [SerializeField] private bool grabbed = false;
-    [SerializeField] private int descendSpeed;
+    [SerializeField] private GameObject _target;
+    [SerializeField] private GameObject _grabbedObject;
+    [SerializeField] private FixedJoint _joint;
+
+    [SerializeField] private bool _fired = false;
+    [SerializeField] private bool _grabbed = false;
+    [SerializeField] private int _descendSpeed;
     [SerializeField] private float _originalStartY;
 
-    private bool touching = false;
     private Vector3 _mousePos;
     private Vector3 hitpoint;
     private RaycastHit hit;
 
-    private void Start()
-    {
-        Vector3 startPos = transform.position;
-        startPos.y = _originalStartY;
-        transform.position = startPos;
-    }
     public override void Fire()
     {
-        fired = true;
+        _fired = true;
     }
     private void Update()
     {
-        if (fired)
+        if (_fired)
         {
-            if (grabbedObject == null)
+            if (_grabbedObject == null)
             {
-                transform.Translate(Vector3.down * Time.deltaTime * descendSpeed);
+                transform.Translate(Vector3.down * Time.deltaTime * _descendSpeed);
             }
             else
             {
-                Destroy(grabbedObject.GetComponent<FixedJoint>());
-                //grabbedObject.GetComponent<NPCPathing>().enabled = true;
-                grabbedObject.GetComponent<NavMeshAgent>().enabled = false;
-                grabbedObject.GetComponent<Animator>().enabled = true;
-                grabbedObject.GetComponent<Rigidbody>().useGravity = true;
-                grabbedObject.GetComponent<RagdollOnOffController>().enabled = true;
-                grabbedObject = null;
-                fired = false;
+                Destroy(_grabbedObject.GetComponent<FixedJoint>());
+
+                _grabbedObject.GetComponent<NavMeshAgent>().enabled = false;
+                _grabbedObject.GetComponent<Animator>().enabled = true;
+                _grabbedObject.GetComponent<Rigidbody>().useGravity = true;
+                _grabbedObject.GetComponent<RagdollOnOffController>().enabled = true;
+                _grabbedObject = null;
+
+                _fired = false;
             }
         }
-        else if (grabbed)
+        else if (_grabbed)
         {
-            transform.Translate(-Vector3.down * Time.deltaTime * descendSpeed);
+            transform.Translate(-Vector3.down * Time.deltaTime * _descendSpeed);
+
             if (transform.position.y > _originalStartY)
-                grabbed = false;
+            {
+                _grabbed = false;
+            }    
         }
         else
         {
@@ -62,26 +59,30 @@ public class Claw : Weapon
             {
                 return;
             }
+
+            _target.transform.position = hit.point;
+
+            this.transform.position = hit.point;
             hitpoint = hit.point;
-            hitpoint.y = transform.position.y;
+            hitpoint.y = _originalStartY;
             transform.position = hitpoint;
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        fired = false;
-        grabbed = true;
+        _fired = false;
+        _grabbed = true;
 
         if (collision.gameObject.GetComponent<RagdollOnOffController>())
         {
-            grabbedObject = collision.gameObject;
+            _grabbedObject = collision.gameObject;
             //grabbedObject.GetComponent<NPCPathing>().enabled = false;
-            grabbedObject.GetComponent<Rigidbody>().useGravity = false;
-            grabbedObject.GetComponent<NavMeshAgent>().enabled = false;
-            grabbedObject.GetComponent<Animator>().enabled = false;
-            grabbedObject.AddComponent<FixedJoint>();
-            grabbedObject.GetComponent<FixedJoint>().connectedBody = this.GetComponent<Rigidbody>();
-            grabbedObject.GetComponent<RagdollOnOffController>().enabled = false;
+            _grabbedObject.GetComponent<Rigidbody>().useGravity = false;
+            _grabbedObject.GetComponent<NavMeshAgent>().enabled = false;
+            _grabbedObject.GetComponent<Animator>().enabled = false;
+            _grabbedObject.AddComponent<FixedJoint>();
+            _grabbedObject.GetComponent<FixedJoint>().connectedBody = this.GetComponent<Rigidbody>();
+            _grabbedObject.GetComponent<RagdollOnOffController>().enabled = false;
         }
     }
 }
--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -1,21 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerWeaponController : MonoBehaviour
 {
     [SerializeField] private Weapon _claw;
     [SerializeField] private Weapon _mallet;
-    [SerializeField] private GameObject _target;
-    [SerializeField] private int _yHitOfset = 15;
-    [SerializeField] private int _clawSpeed = 15;
 
     private Weapon _activeWeapon;
     private Camera _mainCamera;
 
     private Vector3 _mousePos;
-    private Vector3 _hitpos;
-    
     private RaycastHit hit;
 
     void Start()
@@ -23,33 +19,12 @@ public class PlayerWeaponController : MonoBehaviour
         _mainCamera = Camera.main;
         _activeWeapon = _mallet;
     }
-    void Update()
-    {
-        _mousePos = Input.mousePosition;
-        if (!Physics.Raycast(Camera.main.ScreenPointToRay(_mousePos), out hit))
-        {
-            return;
-        }
 
-        _hitpos = hit.point;
-
-        if (_activeWeapon == _mallet)
-        {
-            _hitpos.y = hit.point.y + _yHitOfset;
-        }
-        else
-        {
-            _hitpos.y = _activeWeapon.transform.position.y;
-        }
-
-        Vector3 newPosTarget = _activeWeapon.transform.position;
-        newPosTarget.y = _yHitOfset;
-        //_target.transform.position = newPosTarget;
-    }
     public void OnFire()
     {
         _activeWeapon.Fire();
     }
+
     public void OnSwitchWeapon()
     {
         if (_activeWeapon == _mallet)
