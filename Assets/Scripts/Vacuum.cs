a54609d12084c73f0e20edb5272e713d04d3befe
DenizhanYagci <denizhan1999@hotmail.com>
2024-10-22 10:30:13 -0700

    Added Ufo pull Ray

--- Assets/Scripts/Vacuum.cs
+++ Assets/Scripts/Vacuum.cs
@@ -11,10 +11,14 @@ public class Vacuum : MonoBehaviour
     private bool _vacuumOn = false;
     [SerializeField]private CapsuleCollider _capsuleCollider;
     [SerializeField]private CapsuleCollider _capsuleColliderb;
-    [SerializeField]private VisualEffect _ufoRay;
+    [SerializeField]private GameObject _Ray;
     Vector3 endScale;
+    Vector3 RayStartScale;
+    [SerializeField] float raychangespeed;
     private void Start()
     {
+        RayStartScale = _Ray.transform.localScale;
+        _Ray.transform.localScale = Vector3.zero;
         endScale = new Vector3(3,1,3);
         pulledObjects = new List<GameObject>();
         capturedObjects = new List<GameObject>();
@@ -23,14 +27,21 @@ public class Vacuum : MonoBehaviour
         _vacuumableObjects |= (1 << LayerMask.NameToLayer("Civilian"));
         _vacuumableObjects |= (1 << LayerMask.NameToLayer("Bomb"));
     }
-
+    private void Update()
+    {
+        if (_vacuumOn)
+        {
+            _Ray.transform.localScale = Vector3.Lerp(_Ray.transform.localScale, RayStartScale, raychangespeed * Time.deltaTime);
+        }
+        else
+        {
+            _Ray.transform.localScale = Vector3.Lerp(_Ray.transform.localScale, Vector3.zero, raychangespeed * Time.deltaTime);
+        }
+    }
     private void FixedUpdate()
     {
         if (_vacuumOn)
         {
-            Debug.Log("xd");
-            Vector3.Lerp(_ufoRay.GetVector3("RayScale"), _ufoRay.GetVector3("RayScale") * 3, 3f);
-            _ufoRay.SetVector3("RayScale",Vector3.zero);
             foreach (GameObject enemy in pulledObjects)
             {
                 Vector3 pullForce = (this.gameObject.transform.position - enemy.transform.position).normalized/ Vector3.Distance(this.gameObject.transform.position, enemy.transform.position) * 100;
