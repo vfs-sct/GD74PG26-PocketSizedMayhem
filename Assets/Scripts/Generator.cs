c24d1085f168b5804c3a7ab55629440b630334e1
DenizhanYagci <denizhan1999@hotmail.com>
2024-11-02 18:38:17 -0700

    Added cable electricity VFX

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -7,11 +7,9 @@ using UnityEngine.UI;
 public class Generator : MonoBehaviour
 {
     [SerializeField] private GameObject _spark;
-    [SerializeField] private Image _fillBar;
+    [SerializeField] private List<GameObject> _shields;
+    [SerializeField] private float health;
 
-    [SerializeField] List<GameObject> _shields;
-
-    [SerializeField]private float health;
     float fade = 0;
     float timer = 0;
     void Start()
@@ -21,26 +19,20 @@ public class Generator : MonoBehaviour
 
     void Update()
     {
-        _fillBar.fillAmount = health/100;
         if (health <= 0)
         {
-            
             health = 0;
-            _spark.SetActive(true);
+            _spark.SetActive(false);
             fade = Mathf.Lerp(0,1,timer);
             _shields[0].GetComponent<Renderer>().material.SetFloat("_Fade", fade );
             timer += Time.deltaTime/2;
-        }
-        else
-        {
-            _spark.SetActive(false);
         }
     }
 
     private void OnParticleCollision(GameObject other)
     {
         health--;
-        health--;
+        Mathf.Clamp(health, 0, 100);
     }
 
 
