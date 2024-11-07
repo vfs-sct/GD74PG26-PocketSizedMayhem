ee0e1a2331b2c013970a2ba1fcbceb7b3f2d3617
DenizhanYagci <denizhan1999@hotmail.com>
2024-11-07 07:26:21 -0800

    Added tracker for civilians inside building

---
+++ Assets/CivilianFill.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianFill : MonoBehaviour
+{
+    [SerializeField] private int _civilianInside;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Civilian"))
+        {
+            _civilianInside++;
+        }
+    }
+}
