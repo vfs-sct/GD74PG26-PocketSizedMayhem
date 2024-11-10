dec98c308bf4485063b99eb33b6fbbbac270f4fc
DenizhanYagci <denizhan1999@hotmail.com>
2024-11-09 18:42:25 -0800

    Added tree repair

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,38 @@
+using PrimeTween;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    [SerializeField] private float _respawnTimer;
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private Vector3 _treePosition;
+    [SerializeField] private Quaternion _treeRotation;
+    [SerializeField] private int _bouncePower;
+
+    private void Start()
+    {
+        _treePosition = transform.position;
+        _treeRotation = transform.rotation;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Mallet")
+        {
+            _rigidBody.AddForce((other.gameObject.transform.position - gameObject.transform.position).normalized * _bouncePower * -1 + Vector3.up * 10, ForceMode.Impulse);
+            StartCoroutine(RespawmPieces());
+        }
+    }
+
+    IEnumerator RespawmPieces()
+    {
+        yield return new WaitForSeconds(3f);
+        _rigidBody.isKinematic = true;
+        Tween.Position(this.gameObject.transform, _treePosition, duration: 2);
+        Tween.Rotation(this.gameObject.transform, _treeRotation, duration: 2);
+        yield return new WaitForSeconds(2);
+        _rigidBody.isKinematic = false;
+    }
+}
