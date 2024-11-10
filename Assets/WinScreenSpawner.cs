681e46e2e9c3e5f2f502d7ce91c3fdea451e57b4
DenizhanYagci <denizhan1999@hotmail.com>
2024-11-10 13:02:11 -0800

    Basic win screen added

--- Assets/WinScreenSpawner.cs
+++ Assets/WinScreenSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WinScreenSpawner : MonoBehaviour
@@ -9,6 +10,16 @@ public class WinScreenSpawner : MonoBehaviour
     [SerializeField] private GameObject _hardCivilianRagdoll;
     [SerializeField] private GameObject _negativeCivilianRagdoll;
     [SerializeField] private Transform _spawnLocation;
+
+    [SerializeField] private TextMeshProUGUI _easyCivilianText;
+    [SerializeField] private TextMeshProUGUI _mediumCivilianText;
+    [SerializeField] private TextMeshProUGUI _hardCivilianText;
+    [SerializeField] private TextMeshProUGUI _negativeCivilianText;
+
+    private int easyKilled = 0;
+    private int mediumKilled = 0;
+    private int hardKilled = 0;
+    private int negativeKilled = 0;
     void Start()
     {
         StartCoroutine(SpawmRagdolls());
@@ -19,24 +30,28 @@ public class WinScreenSpawner : MonoBehaviour
         for (int i = 0; i < PlayerStats.EasyCivilianKilled; i++)
         {
             GameObject obj = Instantiate(_easyCivilianRagdoll, _spawnLocation.position, _easyCivilianRagdoll.transform.rotation);
+            _easyCivilianText.text = "Easy Civilian Killed: " + ++easyKilled;
             yield return new WaitForSeconds(0.05f);
         }
         yield return new WaitForSeconds(1f);
         for (int i = 0; i < PlayerStats.MediumCivilianKilled; i++)
         {
             GameObject obj = Instantiate(_mediumCivilianRagdoll, _spawnLocation.position, _mediumCivilianRagdoll.transform.rotation);
+            _mediumCivilianText.text = "Medium Civilian Killed: " + ++mediumKilled;
             yield return new WaitForSeconds(0.05f);
         }
         yield return new WaitForSeconds(1f);
         for (int i = 0; i < PlayerStats.HardCivilianKilled; i++)
         {
             GameObject obj = Instantiate(_hardCivilianRagdoll, _spawnLocation.position, _hardCivilianRagdoll.transform.rotation);
+            _hardCivilianText.text = "Hard Civilian Killed: " + ++hardKilled;
             yield return new WaitForSeconds(0.05f);
         }
         yield return new WaitForSeconds(1f);
         for (int i = 0; i < PlayerStats.NegativeCivilianKilled; i++)
         {
             GameObject obj = Instantiate(_negativeCivilianRagdoll, _spawnLocation.position, _negativeCivilianRagdoll.transform.rotation);
+            _negativeCivilianText.text = "Negative Civilian Killed: " + ++negativeKilled;
             yield return new WaitForSeconds(0.05f);
         }
         yield return null;
